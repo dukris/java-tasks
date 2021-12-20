fb801ac25a4a1306965352d97b259daef0cad86f
Ann Izmailovich <theannalios@gmail.com>
2021-12-20 21:58:28 +0300

    Added files for collections1

---
+++ by/epam/lab/Purchase.java
@@ -0,0 +1,64 @@
+package by.epam.lab;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Purchase {
+    private final String name;
+    private final Byn price;
+    private final int number;
+
+    public Purchase() {
+        this.name = "";
+        this.price = new Byn(0);
+        this.number = 0;
+    }
+
+    public Purchase(String name, Byn price, int number) {
+        this.name = name;
+        this.price = price;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Byn getPrice() {
+        return price;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Byn getCost() {
+        Byn byn = new Byn(price);
+        return byn.mul(number);
+    }
+
+    protected String fieldsToString() {
+        return this.getClass().getSimpleName() + ";" + name + ";" + price + ";" + number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Purchase)) return false;
+        Purchase purchase = (Purchase) o;
+        return price.equals(purchase.price);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = price == null ? 0 : price.hashCode();
+        result = 31 * result + (name == null ? 0 : name.hashCode());
+        return result;
+
+    }
+
+    @Override
+    public String toString() {
+        return fieldsToString() + ";" + getCost();
+    }
+}
