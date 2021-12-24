6d9e563843e39e0d88f875062432830323b74d9d
Ann Izmailovich <theannalios@gmail.com>
2021-12-24 21:04:18 +0300

    Added files for collections1 - [Guide]

---
+++ by/epam/lab/PricePurchase.java
@@ -0,0 +1,24 @@
+package by.epam.lab;
+
+public class PricePurchase extends Purchase {
+    private Byn discount;
+
+    public PricePurchase() {
+
+    }
+
+    public PricePurchase(String name, Byn price, int number, Byn discount) {
+        super(name, price, number);
+        this.discount = discount;
+    }
+
+    @Override
+    public Byn getCost() {
+        return super.getCost().sub(discount.mul(getNumber()));
+    }
+
+    @Override
+    protected String fieldsToString() {
+        return super.fieldsToString() + ";" + discount;
+    }
+}
